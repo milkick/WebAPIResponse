a3421d893d496c7443a18b2d66810c1b2ee286c9
homePC <himabito@hotmail.com>
2018-07-08 22:48:51 +0900

    add Comment test

--- WebAPIResponse/WebAPIResponse/Controllers/ValuesController.cs
+++ WebAPIResponse/WebAPIResponse/Controllers/ValuesController.cs
@@ -12,6 +12,7 @@ namespace WebAPIResponse.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
+            // test
             return new string[] { "value1", "value2" };
         }
 
